db5e75be403ad4a6b17e98ed350c4f4fb74365da
Minh <minh.tranimddtcc@hcmut.edu.vn>
2022-06-02 22:48:09 +0700

    minh

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import 'react-native-gesture-handler';
-import { setCustomText } from 'react-native-global-props';
-import { Provider } from 'react-redux';
-import Toast, { BaseToast } from 'react-native-toast-message';
+import {setCustomText} from 'react-native-global-props';
+import {Provider} from 'react-redux';
+import Toast, {BaseToast} from 'react-native-toast-message';
 
 import Routes from './apps/routes/Routes';
 import store from './apps/redux/store';
-
 
 export default function App() {
   setCustomText({
@@ -17,34 +16,52 @@ export default function App() {
     },
   });
 
-    return (
-        <Provider store={store}>
-            <Routes/>
-            <Toast
-                config={toastConfig}
-            />
-        </Provider>
-    );
-};
-
+  return (
+    <Provider store={store}>
+      <Routes />
+      <Toast config={toastConfig} />
+    </Provider>
+  );
+}
 
 const toastConfig = {
-  success: (props) => (
-      <BaseToast
-          {...props}
-          style={{ borderLeftColor: '#148F00' }}
-          contentContainerStyle={{ paddingHorizontal: 15 }}
-          text1Style={{
-              fontFamily: 'Roboto',
-              fontSize: 20,
-              fontWeight: '700'
-          }}
-          text2Style={{
-              fontFamily: 'Roboto',
-              fontSize: 16,
-              fontWeight: '500',
-              color: '#777777',
-          }}
-      />
-  )
-}
+  success: props => (
+    <BaseToast
+      {...props}
+      style={{borderLeftColor: '#148F00'}}
+      contentContainerStyle={{paddingHorizontal: 15}}
+      text1Style={{
+        fontFamily: 'Roboto',
+        fontSize: 20,
+        fontWeight: '700',
+      }}
+      text2Style={{
+        fontFamily: 'Roboto',
+        fontSize: 16,
+        fontWeight: '500',
+        color: '#777777',
+      }}
+    />
+  ),
+  info: props => (
+    <BaseToast
+      {...props}
+      style={{borderLeftColor: 'lightblue'}}
+      contentContainerStyle={{
+        paddingHorizontal: 15,
+        overflow: 'scroll',
+      }}
+      text1Style={{
+        fontFamily: 'Roboto',
+        fontSize: 20,
+        fontWeight: '700',
+      }}
+      text2Style={{
+        fontFamily: 'Roboto',
+        fontSize: 16,
+        fontWeight: '500',
+        color: '#777777',
+      }}
+    />
+  ),
+};
--- apps/screens/Contact/AddNewMessage.js
+++ apps/screens/Contact/AddNewMessage.js
@@ -6,10 +6,15 @@ import {
   TouchableWithoutFeedback,
   SafeAreaView,
   Keyboard,
+  SectionList,
+  TextInput,
 } from 'react-native';
 import React from 'react';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
-import {colors} from '../../scripts/color'
+import {Avatar} from '@rneui/base';
+import Toast, {BaseToast} from 'react-native-toast-message';
+
+import {colors} from '../../scripts/color';
 
 function Header({navigation}) {
   // function goBack() {
@@ -33,13 +38,151 @@ function Header({navigation}) {
     </View>
   );
 }
+const data = [
+  {
+    title: 'Your contacts',
+    data: [
+      {ava: '', name: 'Mr Đụt'},
+      {ava: '', name: 'Duke'},
+    ],
+  },
+  {
+    title: 'More people on the net',
+    data: [
+      {ava: '', name: 'Mr Đàm'},
+      {ava: '', name: 'Mr Bean'},
+    ],
+  },
+];
+
+function Card(props) {
+  const check = props.index === props.section.data.length - 1;
+  const navigation = props.navigation;
+  return (
+    <TouchableOpacity
+      onPress={() => {
+        navigation.navigate('Chat', {userName: props.name, ava: props.ava});
+      }}
+      style={[
+        styles.item,
+        check
+          ? {borderBottomColor: colors.lightGray, borderBottomWidth: 1}
+          : null,
+      ]}>
+      <Avatar
+        //   activeOpacity={0.2}
+        // avatarStyle={styles.userImg}
+        containerStyle={styles.ava}
+        //   icon={{}}
+        //   iconStyle={styles.icon}
+        //   imageProps={{}}
+        //   onLongPress={() => alert("onLongPress")}
+        //   onPress={() => alert("onPress")}
+        //   overlayContainerStyle={{}}
+        //   placeholderStyle={{}}
+        rounded
+        size="medium"
+        source={props.ava == '' ? null : {uri: props.ava}}
+        title={props.name[0]}
+        //   titleStyle={{}}
+      />
+      <View style={check ? styles.itemTextLastChild : styles.itemText}>
+        <Text style={styles.textHeader}>{props.name}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+}
 
 export default function AddNewMessage({navigation}) {
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
       <SafeAreaView style={styles.screen}>
         <Header navigation={navigation} />
-        <View style={styles.searchBar}></View>
+        <View style={styles.searchBar}>
+          <View style={styles.row}>
+            <Text style={styles.label}>Name:</Text>
+            <TextInput
+              editable
+              placeholder="Enter contact's name here..."
+              // placeholderTextColor={}
+              // value={username}
+              // // returnKeyType='next'
+              // // autoFocus={true}
+              // // onSubmitEditing={() => {}}
+              // onFocus={() => dispatch(setFlag(true))}
+              // onBlur={() => dispatch(setFlag(false))}
+              // onChangeText={(value) => setUsername(value)}
+              // blurOnSubmit={false}
+              // ref={refInputSearch}
+              onChangeText={value => {}}
+              style={styles.input}
+            />
+          </View>
+          <Text style={styles.linkText}>or</Text>
+          <View style={styles.row}>
+            <Text style={styles.label}>ID:</Text>
+            <TextInput
+              editable
+              placeholder="Enter contact's ID here..."
+              // placeholderTextColor={}
+              // value={username}
+              // // returnKeyType='next'
+              // // autoFocus={true}
+              // // onSubmitEditing={() => {}}
+              // onFocus={() => dispatch(setFlag(true))}
+              // onBlur={() => dispatch(setFlag(false))}
+              // onChangeText={(value) => setUsername(value)}
+              // blurOnSubmit={false}
+              // ref={refInputSearch}
+              onChangeText={value => {}}
+              style={styles.input}
+            />
+            <TouchableOpacity
+              onPress={() => {
+                Toast.show({
+                  type: 'info',
+                  text1: 'How to get ID ?',
+                  text2:
+                    'The ID of a contact can be found in their Profile section, and so is yours.',
+                  position: 'top',
+                });
+              }}>
+              <FontAwesome5
+                name="question-circle"
+                color="#0C88FA"
+                size={35}
+                solid
+                style={[styles.iconButton, {marginLeft: 10}]}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+        <SectionList
+          sections={data}
+          renderItem={({item, index, section}) => (
+            <Card
+              {...item}
+              index={index}
+              section={section}
+              navigation={navigation}
+            />
+          )}
+          renderSectionHeader={({section: {title}}) => (
+            <Text style={styles.heading}>{title}</Text>
+          )}
+          // renderSectionHeader={({ section: { title } }) => (
+          //     <Text style={styles.heading}>{title}</Text>
+          //   )}
+          // onEndReachedThreshold={0.3}
+          // onEndReached={()=>{
+          //     setIsRefreshing(true);
+          //     dbCtx.fetchDeviceLog(15,()=>setIsRefreshing(false))
+          // }}
+          // onRefresh={()=>{
+          //     setIsRefreshing(true);
+          // }}
+          // refreshing={isRefreshing}
+        />
       </SafeAreaView>
     </TouchableWithoutFeedback>
   );
@@ -71,9 +214,69 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     textAlignVertical: 'center',
   },
-  searchBar:{
-    flex: 1,
+  searchBar: {
     backgroundColor: colors.primary,
     height: 115,
-  }
+    padding: 10,
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    height: 35,
+  },
+  label: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  input: {
+    fontSize: 16,
+    padding: 5,
+    marginLeft: 10,
+    paddingLeft: 15,
+    flex: 1,
+    backgroundColor: 'white',
+    borderRadius: 35,
+    height: 35,
+  },
+  linkText: {
+    fontSize: 16,
+    color: 'white',
+    fontStyle: 'italic',
+  },
+  item: {
+    paddingVertical: 10,
+    flexDirection: 'row',
+    // backgroundColor: 'green'
+  },
+  ava: {
+    marginLeft: 15,
+    // alignSelf: 'center',
+    backgroundColor: 'grey',
+    borderColor: colors.background,
+    borderWidth: 1,
+  },
+  itemTextLastChild: {
+    flex: 1,
+    paddingVertical: 15,
+
+    // backgroundColor: 'blue'
+  },
+  itemText: {
+    flex: 1,
+    borderBottomColor: colors.lightGray,
+    borderBottomWidth: 1,
+    paddingVertical: 15,
+  },
+  heading: {
+    fontWeight: '900',
+    fontSize: 18,
+    marginVertical: 10,
+    marginLeft: 20,
+  },
+  textHeader: {
+    fontSize: 18,
+    fontWeight: '700',
+    marginLeft: 15,
+  },
 });
